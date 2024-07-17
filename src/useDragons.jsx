32ddcd6ccc696b02b30d4c1f9cabf641f19debe1
Success Ambaliya <success.ambaliya13@gmail.com>
2024-07-17 13:19:31 -0400

    Update useDragons.jsx

--- src/useDragons.jsx
+++ src/useDragons.jsx
@@ -1,16 +1,16 @@
-//useDragon hook
+// useDragons.jsx
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 
-export default function useDragons(initialAmount = 0) {
+export default function useDragons() {
   const [dragons, setDragons] = useState([]);
   const POSSIBLE_DRAGONS = 40;
 
   const getDragon = async ({ id }) => {
     try {
       const res = await fetch(
-        `https://house-of-the-dragon-api.vercel.app/characters/${id}`
+        https://house-of-the-dragon-api.vercel.app/characters/${id}
       );
       if (!res.ok) {
         throw new Error("Failed to fetch character data");
@@ -28,10 +28,12 @@ export default function useDragons(initialAmount = 0) {
     let tries = 0;
     const isFirstVisit = localStorage.getItem("visited") === null;
     if (isFirstVisit) localStorage.setItem("visited", true);
-
+    // Loop until desired amount of unique dragon characters are selected or max tries reached
     while (dragonsToShow.length < amount && tries < 100) {
       const randomId = Math.floor(Math.random() * POSSIBLE_DRAGONS) + 1;
+
       const dragon = await getDragon({ id: randomId });
+      // Check if the fetch was successful and the ID isn't already in the list
       if (dragon && !dragonsToShow.some(({ id }) => id === dragon.id)) {
         dragonsToShow.push(dragon);
       } else {
@@ -40,7 +42,6 @@ export default function useDragons(initialAmount = 0) {
     }
     return dragonsToShow;
   };
-
   function shuffleDragons() {
     const availableDragons = [...dragons];
     const shuffledDragons = [];
@@ -48,6 +49,7 @@ export default function useDragons(initialAmount = 0) {
     while (availableDragons.length) {
       const index = Math.floor(Math.random() * availableDragons.length);
       const dragon = availableDragons[index];
+      // Ensure each dragon has a unique key/id for React's rendering
       dragon.id = uniqid();
       shuffledDragons.push(dragon);
       availableDragons.splice(index, 1);
@@ -55,15 +57,6 @@ export default function useDragons(initialAmount = 0) {
 
     setDragons(shuffledDragons);
   }
-
-  useEffect(() => {
-    if (initialAmount > 0) {
-      (async () => {
-        const initialDragons = await getRandomDragons(initialAmount);
-        setDragons(initialDragons);
-      })();
-    }
-  }, [initialAmount]);
 
   return { dragons, getRandomDragons, shuffleDragons, setDragons };
 }
